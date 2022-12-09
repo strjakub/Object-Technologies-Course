a46b9a1d9b34666980b36e69b7d38924df982839
Kawron <karol.wrona01@gmail.com>
2022-12-09 17:31:06 +0100

    added constructors

--- Backend/src/main/java/com/example/backend/controller/ThumbnailController.java
+++ Backend/src/main/java/com/example/backend/controller/ThumbnailController.java
@@ -7,19 +7,21 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.awt.*;
-
-@RestController
+@RestController("thumbnail")
 public class ThumbnailController {
 
     private ThumbnailService service;
 
-    @GetMapping(path = "/get/{id}")
+    public ThumbnailController(ThumbnailService service) {
+        this.service = service;
+    }
+
+    @GetMapping(path = "{id}")
     public ResponseEntity<Thumbnail> getImage(@PathVariable String id) {
         return new ResponseEntity<>(service.getThumbnail(Integer.parseInt(id)), HttpStatus.OK);
     }
 
-    @PostMapping("post")
+    @PostMapping()
     public ResponseEntity<Integer> postImage(@RequestBody Image imageToBeProcessed) {
         int id = service.uploadPhoto(imageToBeProcessed);
         return new ResponseEntity<>(id, HttpStatus.OK);
