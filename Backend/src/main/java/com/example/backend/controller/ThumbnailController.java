45c374dc2df4856ccf6f6e92de7d122bad0ed557
Kawron <karol.wrona01@gmail.com>
2022-12-09 17:17:14 +0100

    Added custom error

--- Backend/src/main/java/com/example/backend/controller/ThumbnailController.java
+++ Backend/src/main/java/com/example/backend/controller/ThumbnailController.java
@@ -1,5 +1,6 @@
 package com.example.backend.controller;
 
+import com.example.backend.model.Image;
 import com.example.backend.model.Thumbnail;
 import com.example.backend.service.ThumbnailService;
 import org.springframework.http.HttpStatus;
@@ -15,12 +16,12 @@ public class ThumbnailController {
 
     @GetMapping(path = "/get/{id}")
     public ResponseEntity<Thumbnail> getImage(@PathVariable String id) {
-        return new ResponseEntity<>(new Thumbnail(), HttpStatus.OK);
+        return new ResponseEntity<>(service.getThumbnail(Integer.parseInt(id)), HttpStatus.OK);
     }
 
     @PostMapping("post")
     public ResponseEntity<Integer> postImage(@RequestBody Image imageToBeProcessed) {
-        int id = 0;
+        int id = service.uploadPhoto(imageToBeProcessed);
         return new ResponseEntity<>(id, HttpStatus.OK);
     }
 }
---
+++ Backend/src/main/java/com/example/backend/error/UploadPhotoError.java
@@ -0,0 +1,4 @@
+package com.example.backend.error;
+
+public class UploadPhotoError extends Throwable {
+}
