16d45158f7dd557d2f54e6a95f4701f5cb47cbd1
Kawron <karol.wrona01@gmail.com>
2022-12-15 19:43:52 +0100

    amend commit

--- Backend/src/main/java/com/example/backend/util/ThumbnailGenerator.java
+++ Backend/src/main/java/com/example/backend/util/ThumbnailGenerator.java
@@ -3,6 +3,7 @@ package com.example.backend.util;
 import com.example.backend.model.Image;
 import com.example.backend.model.Thumbnail;
 import lombok.extern.slf4j.Slf4j;
+import org.imgscalr.Scalr;
 import org.springframework.stereotype.Component;
 
 import javax.imageio.ImageIO;
@@ -29,27 +30,13 @@ public class ThumbnailGenerator {
     private byte[] resize(Image image) throws IOException {
         InputStream inputStream = new ByteArrayInputStream(image.getData());
         BufferedImage inputImage = ImageIO.read(inputStream);
-        Dimension scaledDimension = rescaleKeepingRatio(inputImage);
-        BufferedImage outputImage = new BufferedImage(scaledDimension.width, scaledDimension.height,
-                inputImage.getType());
+
+        BufferedImage outImage = Scalr.resize(inputImage, width);
+
         ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-        ImageIO.write(outputImage, image.getExtension(), outputStream);
-        //TODO figure what should be put as id
+        ImageIO.write(outImage, image.getExtension(), outputStream);
+        log.error(image.getExtension());
         return outputStream.toByteArray();
-    }
-
-    private Dimension rescaleKeepingRatio(BufferedImage inputImage) {
-        double widthRatio = inputImage.getWidth() / (double) width;
-        double heightRatio = inputImage.getHeight() / (double) height;
-
-        int finalWidth = width;
-        int finalHeight = height;
-        if (widthRatio > heightRatio) {
-            finalHeight = (int) (inputImage.getHeight() / heightRatio);
-        } else {
-            finalWidth = (int) (inputImage.getWidth() / widthRatio);
-        }
-        return new Dimension(finalWidth, finalHeight);
     }
 
 }
