bb5dbfef9ba557784348c22cce0b2f29221e6f4b
aksamit <pa4166.m@gmail.com>
2022-12-16 02:53:00 +0100

    frontend

--- lab-gui-gallery/src/main/java/model/Dto.java
+++ lab-gui-gallery/src/main/java/model/Dto.java
@@ -3,9 +3,8 @@ package model;
 import java.util.Base64;
 
 public class Dto {
-    public Integer id;
-    public String data;
-    public String extension;
+    private String data;
+    private String extension;
     
     public static Image convertTo(Dto dto) {
         return new Image(Base64.getDecoder().decode(dto.data), dto.extension);
