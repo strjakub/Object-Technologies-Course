d2ad67bd48eff163cabadca5aa12239375bb0b1d
aksamit <pa4166.m@gmail.com>
2022-12-16 02:13:08 +0100

    frontend

--- lab-gui-gallery/src/main/java/controller/ImageController.java
+++ lab-gui-gallery/src/main/java/controller/ImageController.java
@@ -1,15 +1,17 @@
 package controller;
 
 import java.io.ByteArrayInputStream;
-import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
 
 import javafx.fxml.FXML;
+import javafx.scene.Scene;
 import javafx.scene.control.ProgressIndicator;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 import model.Dto;
 
 import services.NetworkCallback;
@@ -51,16 +53,17 @@ public class ImageController {
             @Override
             public void process(Dto result) throws IOException {
                 var image = Dto.convertTo(result);
-
-                var file = File.createTempFile("ddddd", "." + image.getExtension());
-                try (var fos = new FileOutputStream(file)) {
-                    fos.write(image.getData());
-                }
-                
-                String expr = "rundll32 \"C:\\Program Files (x86)\\Windows Photo Viewer\\PhotoViewer.dll\", ImageView_Fullscreen " + file.getAbsolutePath();
-                Runtime.getRuntime().exec(expr);                
+                var stage = new Stage();
+                var img = new Image(new ByteArrayInputStream(image.getData()));
+                var imageView = new ImageView(img);
+                var box = new HBox();
+                box.getChildren().add(imageView);
+                stage.setScene(new Scene(box));
+                stage.setTitle("Powiekszone zdjecie małego zdjecia");
+                stage.initModality(Modality.WINDOW_MODAL);
+                stage.initOwner(container.getScene().getWindow() );
+                stage.show();
             }
         });
     }
-
 }
