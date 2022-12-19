c6e25b90a0b8d4c3d0cf0cac3d2dd89460abbaa7
aksamit <pa4166.m@gmail.com>
2022-12-19 01:37:44 +0100

    ThumbnailGenerator

--- Backend/src/main/java/com/example/backend/utils/ThumbnailGenerator.java
+++ Backend/src/main/java/com/example/backend/utils/ThumbnailGenerator.java
@@ -11,17 +11,13 @@ import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.Random;
 
 @Component
 @Slf4j
 public class ThumbnailGenerator {
 
     private static final int WIDTH = 300;
-
     private static final int HEIGHT = 300;
-
-    private final Random random = new Random();
 
     public byte[] convertToThumbnail(Image image) throws IOException {
         log.info(String.format("...Converting photo... : size = %s", image.getData().length));
@@ -36,14 +32,6 @@ public class ThumbnailGenerator {
         BufferedImage outImage = Scalr.resize(inputImage, WIDTH, HEIGHT);
         ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
         ImageIO.write(outImage, image.getExtension(), outputStream);
-
-        try {
-            var time = random.nextInt(2, 10) * 1000;
-            Thread.sleep(time);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
-
         return outputStream.toByteArray();
     }
 
