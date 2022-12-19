eb66a001284f13c2b8898c8fd027032f3af458c1
aksamit <pa4166.m@gmail.com>
2022-12-19 01:58:32 +0100

    FileService

--- lab-gui-gallery/src/main/java/services/FileService.java
+++ lab-gui-gallery/src/main/java/services/FileService.java
@@ -4,15 +4,30 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
+import java.util.stream.Stream;
 import java.util.zip.ZipFile;
 
 import model.Picture;
 
 public class FileService implements IFileService {
 
-    private final IRetrofitService retrofitService = RetrofitService.Instance;
-    private final IPhotosService photosService = new PhotosService();
+    private static final List<String> photosExtensions = Arrays.asList("png", "jpg", "bmp");
+    private static final String ZIP = "zip";
+
+    public boolean isPhotoOrZip(String extension) {
+        return extension.equals(ZIP) || isPhoto(extension);
+    }
+
+    public boolean isPhoto(String extension) {
+        return photosExtensions.contains(extension);
+    }
+
+    public String[] getExtensionFilter() {
+        return Stream.concat(Stream.of(ZIP), photosExtensions.stream()).map(x -> String.format("*.%s", x)).toArray(String[]::new);
+    }
 
     public String getExtension(String fileName) {
         var i = fileName.lastIndexOf('.');
@@ -26,34 +41,54 @@ public class FileService implements IFileService {
     public void postFile(File file, NetworkCallback<Integer> callback) throws IOException {
         var absolutePath = file.getAbsolutePath();
         var extension = getExtension(absolutePath);
-
-        if (extension.equals("zip")) {
-            var zip = new ZipFile(absolutePath);
-            for (var entry: Collections.list(zip.entries())) {
-                var ext = getExtension(entry.getName());
-                if (photosService.isPhoto(ext)) {
-                    var stream = zip.getInputStream(entry);
-                    var bytes = stream.readAllBytes();
-                    sendOneImage(bytes, ext, callback);
-                }
-            }
-            zip.close();
-            return;
-        }
-
-        sendOneImage(absolutePath, callback);
+        FileSenderStrategyBuilder.Build(extension).sendFile(absolutePath, callback);
     }
+}
 
-    private void sendOneImage(byte[] bytes, String extension, NetworkCallback<Integer> callback) throws IOException {
+interface IFileSender {
+    void sendFile(String fullPath, NetworkCallback<Integer> callback) throws IOException;
+}
+
+class PhotoFileSender implements IFileSender {
+
+    private final IFileService fileService = new FileService();
+    private final IRetrofitService retrofitService = RetrofitService.Instance;
+
+    @Override
+    public void sendFile(String fullPath, NetworkCallback<Integer> callback) throws IOException {
+        var extension = fileService.getExtension(fullPath);
+        var path  = Paths.get(fullPath);
+        var bytes = Files.readAllBytes(path);
         var image = new Picture(bytes, extension);
         retrofitService.postImage(image, callback);
     }
+}
 
-    private void sendOneImage(String fullPath, NetworkCallback<Integer> callback) throws IOException {
-        var extension = getExtension(fullPath);
-        var path  = Paths.get(fullPath);
-        var bytes = Files.readAllBytes(path);
-        sendOneImage(bytes, extension, callback);
+class ZipFileSender implements IFileSender {
+
+    private final IFileService fileService = new FileService();
+    private final IRetrofitService retrofitService = RetrofitService.Instance;
+
+    @Override
+    public void sendFile(String fullPath, NetworkCallback<Integer> callback) throws IOException {
+        var zip = new ZipFile(fullPath);
+        for (var entry: Collections.list(zip.entries())) {
+            var extension =  fileService.getExtension(entry.getName());
+            if (fileService.isPhoto(extension)) {
+                var stream = zip.getInputStream(entry);
+                var bytes = stream.readAllBytes();
+                var image = new Picture(bytes, extension);
+                retrofitService.postImage(image, callback);
+            }
+        }
+        zip.close();
     }
 
+}
+
+class FileSenderStrategyBuilder {
+
+    public static IFileSender Build(String extension) {
+        return extension.equals("zip") ? new ZipFileSender() : new PhotoFileSender();
+    }
 }
--- lab-gui-gallery/src/main/java/services/IPhotosService.java
+++
@@ -1,7 +0,0 @@
-package services;
-
-public interface IPhotosService {
-    boolean isPhoto(String extension);
-    boolean isPhotoOrZip(String extension);
-    String[] getExtensionFilter();
-}
