8e2838d9fb2183ceca2615a2223e6cc7d320f2fa
Kawron <karol.wrona01@gmail.com>
2022-12-22 00:09:10 +0100

    problems

--- Backend/src/main/java/com/example/backend/service/ImageService.java
+++ Backend/src/main/java/com/example/backend/service/ImageService.java
@@ -2,11 +2,9 @@ package com.example.backend.service;
 
 import com.example.backend.model.Image;
 import com.example.backend.repositories.ImageRepository;
+import io.reactivex.rxjava3.core.Maybe;
 import io.reactivex.rxjava3.core.Single;
 import org.springframework.stereotype.Service;
-
-import javax.persistence.EntityNotFoundException;
-import java.util.Optional;
 
 @Service
 public class ImageService {
@@ -19,19 +17,10 @@ public class ImageService {
     }
 
     public Single<Integer> uploadImage(Image image) {
-        return Single.create(subscriber -> {
-            Image res = imageRepository.save(image);
-            subscriber.onSuccess(res.getId());
-        });
+        return Single.fromCallable(() -> imageRepository.save(image).getId());
     }
 
-    public Single<Image> getImage(int id) {
-        return Single.create(subscriber -> {
-            Optional<Image> res = imageRepository.findById(id);
-            if (res.isEmpty()) {
-                throw new EntityNotFoundException();
-            }
-            subscriber.onSuccess(res.get());
-        });
+    public Maybe<Image> getImage(int id) {
+        return Maybe.fromOptional(imageRepository.findById(id));
     }
 }
--- Backend/src/main/java/com/example/backend/service/ThumbnailService.java
+++ Backend/src/main/java/com/example/backend/service/ThumbnailService.java
@@ -2,15 +2,16 @@ package com.example.backend.service;
 
 import com.example.backend.model.Image;
 import com.example.backend.model.Thumbnail;
+import com.example.backend.model.ThumbnailOption;
 import com.example.backend.repositories.ImageRepository;
 import com.example.backend.repositories.ThumbnailRepository;
 import com.example.backend.utils.ThumbnailGenerator;
-import io.reactivex.rxjava3.core.Single;
+import io.reactivex.rxjava3.core.Completable;
+import io.reactivex.rxjava3.core.Maybe;
 import io.reactivex.rxjava3.schedulers.Schedulers;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
-import javax.persistence.EntityNotFoundException;
 import java.util.Optional;
 
 @Slf4j
@@ -28,27 +29,20 @@ public class ThumbnailService {
         this.generator = generator;
     }
 
-    public Single<Optional<Thumbnail>> getThumbnail(int id) {
-        return Single.create(subscriber -> {
-            Optional<Image> img = imageRepository.findById(id);
-            if (img.isEmpty()) {
-                throw new EntityNotFoundException();
-            }
-            Optional<Thumbnail> res = thumbnailRepository.findByImage_Id(img.get());
-            if (res.isEmpty()) {
-                subscriber.onSuccess(Optional.empty());
-                return;
-            }
-            subscriber.onSuccess(Optional.of(res.get()));
-        });
+    public Maybe<Thumbnail> getThumbnail(Image image) {
+        return Maybe.fromCallable(() -> {
+                    Optional<Image> img = imageRepository.findById(id);
+                    if (img.isPresent()) {
+                        return new ThumbnailOption(img, ValidQuery.TRUE);
+                    } else {
+                        return new ThumbnailOption(img, ValidQuery.TRUE);
+                    }
+                })
+                .flatMap(Maybe::fromOptional)
+//                .flatMap((img) -> Maybe.fromOptional(thumbnailRepository.findByImage_Id(img)));
     }
 
     public void generateThumbnail(Image img) {
-        Single<Thumbnail> call = Single.create(subscriber -> {
-            Thumbnail thumbnail = new Thumbnail(generator.convertToThumbnail(img), img.getExtension(), img);
-            Thumbnail res = thumbnailRepository.save(thumbnail);
-            subscriber.onSuccess(res);
-        });
-        call.subscribeOn(Schedulers.computation()).subscribe();
+        Completable.fromAction(() -> {}).subscribeOn(Schedulers.computation()).subscribe();
     }
 }
