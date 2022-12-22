307c56a71f8510c37e6b644827d6a75661db2628
Kawron <karol.wrona01@gmail.com>
2022-12-22 16:49:52 +0100

    some fixes for backend

--- Backend/src/main/java/com/example/backend/service/ThumbnailService.java
+++ Backend/src/main/java/com/example/backend/service/ThumbnailService.java
@@ -8,10 +8,12 @@ import com.example.backend.repositories.ThumbnailRepository;
 import com.example.backend.utils.ThumbnailGenerator;
 import io.reactivex.rxjava3.core.Completable;
 import io.reactivex.rxjava3.core.Maybe;
+import io.reactivex.rxjava3.core.Single;
 import io.reactivex.rxjava3.schedulers.Schedulers;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import javax.persistence.EntityNotFoundException;
 import java.util.Optional;
 
 @Slf4j
@@ -29,17 +31,19 @@ public class ThumbnailService {
         this.generator = generator;
     }
 
-    public Maybe<Thumbnail> getThumbnail(Image image) {
-        return Maybe.fromCallable(() -> {
-                    Optional<Image> img = imageRepository.findById(id);
-                    if (img.isPresent()) {
-                        return new ThumbnailOption(img, ValidQuery.TRUE);
-                    } else {
-                        return new ThumbnailOption(img, ValidQuery.TRUE);
-                    }
-                })
-                .flatMap(Maybe::fromOptional)
-//                .flatMap((img) -> Maybe.fromOptional(thumbnailRepository.findByImage_Id(img)));
+    public Single<Optional<Thumbnail>> getThumbnail(int id) {
+        return Single.create(subscriber -> {
+            Optional<Image> img = imageRepository.findById(id);
+            if (img.isEmpty()) {
+                throw new EntityNotFoundException();
+            }
+            Optional<Thumbnail> res = thumbnailRepository.findByImage_Id(img.get());
+            if (res.isEmpty()) {
+                subscriber.onSuccess(Optional.empty());
+                return;
+            }
+            subscriber.onSuccess(Optional.of(res.get()));
+        });
     }
 
     public void generateThumbnail(Image img) {
