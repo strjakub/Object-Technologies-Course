af4dc5c867ccbf79b49a150c6d1e5e822fddc71a
Kawron <karol.wrona01@gmail.com>
2023-01-07 20:43:40 +0100

    pushing before working on milestone 2

--- lab-gui-gallery/src/main/java/services/File.java
+++ lab-gui-gallery/src/main/java/services/File.java
@@ -7,18 +7,18 @@ import java.util.stream.Stream;
 public class File {
 
     public static final String ZIP = "zip";
-    private static final List<String> photosExtensions = Arrays.asList("png", "jpg", "bmp");
+    private static final List<String> PHOTOS_EXTENSIONS = Arrays.asList("png", "jpg", "bmp");
 
     public static boolean isPhotoOrZip(String extension) {
         return extension.equals(ZIP) || isPhoto(extension);
     }
 
     public static boolean isPhoto(String extension) {
-        return photosExtensions.contains(extension);
+        return PHOTOS_EXTENSIONS.contains(extension);
     }
 
     public static String[] getExtensionFilter() {
-        return Stream.concat(Stream.of(ZIP), photosExtensions.stream()).map(x -> String.format("*.%s", x)).toArray(String[]::new);
+        return Stream.concat(Stream.of(ZIP), PHOTOS_EXTENSIONS.stream()).map(x -> String.format("*.%s", x)).toArray(String[]::new);
     }
 
     public static String getExtension(String fileName) {
