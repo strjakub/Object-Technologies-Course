463a971fcf28ff6af02e6da891900e35eb37cb23
DESKTOP-I7NIUE3\stroj <jakubstr@student.agh.edu.pl>
2023-01-09 00:13:05 +0100

    finding images to restart converting

--- Backend/src/main/java/com/example/backend/repositories/ImageRepository.java
+++ Backend/src/main/java/com/example/backend/repositories/ImageRepository.java
@@ -2,8 +2,14 @@ package com.example.backend.repositories;
 
 import com.example.backend.model.Image;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Collection;
 
 @Repository
 public interface ImageRepository extends JpaRepository<Image, Integer> {
+
+    @Query("SELECT i FROM Image i WHERE i NOT IN (SELECT t.image FROM Thumbnail t)")
+    Collection<Image> findNotProcessedImages();
 }
