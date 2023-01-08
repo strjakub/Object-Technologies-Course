af7e37ebf5be0dd46fee7bfa970d83550803d511
Kawron <karol.wrona01@gmail.com>
2023-01-08 13:58:02 +0100

    added full json serialization

--- Backend/src/main/java/com/example/backend/controller/Controller.java
+++ Backend/src/main/java/com/example/backend/controller/Controller.java
@@ -5,7 +5,6 @@ import com.example.backend.model.Thumbnail;
 import com.example.backend.service.ImageService;
 import com.example.backend.service.ThumbnailService;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import io.reactivex.rxjava3.core.Maybe;
 import io.reactivex.rxjava3.core.Single;
 import io.reactivex.rxjava3.schedulers.Schedulers;
 import lombok.extern.slf4j.Slf4j;
@@ -28,19 +27,19 @@ public class Controller {
     }
 
     @GetMapping(path = "{id}")
-    public Single<ResponseEntity<String>> getImage(@PathVariable int id) {
+    public Single<ResponseEntity<Image>> getImage(@PathVariable int id) {
         return imageService.getImage(id).subscribeOn(Schedulers.io())
-                .map(r -> new ResponseEntity<>(mapper.writeValueAsString(r), HttpStatus.OK))
+                .map(r -> new ResponseEntity<>(r, HttpStatus.OK))
                 .defaultIfEmpty(new ResponseEntity<>(null, HttpStatus.NOT_FOUND));
     }
 
     @GetMapping(path = "thumbnail/{id}")
-    public Single<ResponseEntity<String>> getThumbnail(@PathVariable int id) {
+    public Single<ResponseEntity<Thumbnail>> getThumbnail(@PathVariable int id) {
         log.info("GOT REQUEST");
         return thumbnailService.getThumbnail(id).subscribeOn(Schedulers.io())
                 .map(res -> res.isEmpty()
-                        ? new ResponseEntity<>("", HttpStatus.PROCESSING)
-                        : new ResponseEntity<>(mapper.writeValueAsString(res.get()), HttpStatus.OK))
+                        ? new ResponseEntity<>(new Thumbnail(), HttpStatus.PROCESSING)
+                        : new ResponseEntity<>(res.get(), HttpStatus.OK))
                 .onErrorReturnItem(new ResponseEntity<>(null, HttpStatus.NOT_FOUND));
     }
 
