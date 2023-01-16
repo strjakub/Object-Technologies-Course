a5bcf75514d608ed32ee5060cd30321c65defcef
DESKTOP-I7NIUE3\stroj <jakubstr@student.agh.edu.pl>
2023-01-17 00:25:47 +0100

    restart server

--- Backend/src/main/java/com/example/backend/repositories/ImageRepository.java
+++ Backend/src/main/java/com/example/backend/repositories/ImageRepository.java
@@ -2,8 +2,21 @@ package com.example.backend.repositories;
 
 import com.example.backend.model.Image;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Collection;
 
 @Repository
 public interface ImageRepository extends JpaRepository<Image, Integer> {
+
+    @Query("SELECT i FROM Image i WHERE i NOT IN (SELECT t.image FROM Thumbnail t)")
+    Collection<Image> findNotProcessedToSmallImages();
+
+    @Query("SELECT t.image FROM Thumbnail t WHERE t.medium IS NULL")
+    Collection<Image> findNotProcessedToMediumImages();
+
+    @Query("SELECT t.image FROM Thumbnail t WHERE t.large IS NULL AND t.medium IS NOT NULL")
+    Collection<Image> findNotProcessedToLargeImages();
+
 }
