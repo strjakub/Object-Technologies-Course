61c13faaf84e3314a1d11d8eca39419cc26ea204
aksamit <pa4166.m@gmail.com>
2023-01-19 21:31:08 +0100

    FolderController

--- lab-gui-gallery/src/main/java/controller/FolderController.java
+++ lab-gui-gallery/src/main/java/controller/FolderController.java
@@ -1,13 +1,9 @@
 package controller;
 
 import javafx.fxml.FXML;
-import javafx.geometry.Insets;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.Background;
-import javafx.scene.layout.BackgroundFill;
-import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
@@ -41,8 +37,8 @@ public class FolderController implements ImageSizeChangeListener {
         imageView.setOnMouseClicked(event -> { showDirectory(); });
         var label = new Label(folderName);
         label.setTextFill(Color.color(1, 0, 0));
+        label.setOnMouseClicked(event -> { showDirectory(); });
         label.setStyle("-fx-font-weight: bold");
-        label.setBackground(new Background(new BackgroundFill(Color.rgb(0, 0, 80, 0.7), new CornerRadii(5.0), new Insets(-5.0))));
         pane.getChildren().addAll(imageView, label);
         container.getChildren().add(pane);
         steeringController.addListener(this);
