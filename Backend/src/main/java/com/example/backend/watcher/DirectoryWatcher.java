fca8d5e03b4adeb41c55739765074a15a10c000b
Kawron <karol.wrona01@gmail.com>
2023-01-19 20:02:45 +0100

    fixed directory watcher

--- Backend/src/main/java/com/example/backend/watcher/DirectoryWatcher.java
+++ Backend/src/main/java/com/example/backend/watcher/DirectoryWatcher.java
@@ -9,12 +9,15 @@ import io.reactivex.rxjava3.schedulers.Schedulers;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.io.FilenameUtils;
+import org.springframework.retry.annotation.Backoff;
+import org.springframework.retry.annotation.Retryable;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.nio.file.*;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import static java.nio.file.StandardWatchEventKinds.*;
 
@@ -44,33 +47,44 @@ public class DirectoryWatcher {
         resources.register(watchService, ENTRY_CREATE, ENTRY_DELETE);
         return Observable.create(emitter -> {
             WatchKey key;
-            while((key = watchService.take()) != null) {
-                for (WatchEvent<?> event : key.pollEvents()) {
-                    log.warn("Event kind:" + event.kind() + ". File affected: " + event.context() + ".");
-                    emitter.onNext(event);
+            try {
+                while((key = watchService.take()) != null) {
+                    for (WatchEvent<?> event : key.pollEvents()) {
+                        log.info("Event kind:" + event.kind() + ". File affected: " + event.context() + ".");
+                        emitter.onNext(event);
+                    }
+                    key.reset();
                 }
-                key.reset();
+            } catch (InterruptedException ie) {
+                log.error("Watcher got interrupted exception");
+                Thread.currentThread().interrupt();
+                emitter.onComplete();
             }
-            emitter.onComplete();
         });
+    }
+
+    @Retryable(maxAttempts=5,backoff = @Backoff(delay = 3000))
+    private byte[] readFile(Path path) throws IOException {
+        return Files.readAllBytes(path);
     }
 
     public void watch() throws IOException {
         Observable<WatchEvent<?>> source = getSource();
-        source.subscribeOn(Schedulers.io())
+        source.retryWhen(errors -> errors.flatMap(error -> Observable.timer(5, TimeUnit.SECONDS))).subscribeOn(Schedulers.io())
                 .subscribe(res -> {
+                    log.info("Watcher received event");
                    if (res.kind() == ENTRY_CREATE) {
                        if (!validExtensions.contains(FilenameUtils.getExtension(res.context().toString()))) {
                            log.warn("added file with invalid extension");
                        }
                        else {
                            Path imagePath = Paths.get(resources + "/" + res.context().toString()).toAbsolutePath();
-                           byte[] data = Files.readAllBytes(imagePath);
+                           Thread.sleep(1000);
+                           byte[] data = readFile(imagePath);
                            String extension = FilenameUtils.getExtension(res.context().toString());
                            Image newImage = new Image(data, extension, "/server");
                            imageService.uploadImage(newImage).subscribeOn(Schedulers.computation()).subscribe();
                            thumbnailService.generateThumbnail(newImage);
-                           //todo here we can add some messaging to the
                            log.info("Image saved");
                        }
                    }
@@ -80,6 +94,6 @@ public class DirectoryWatcher {
                    else {
                        log.error("Wrong type of file system event");
                    }
-                });
+                }, err -> log.error(err.toString()), () -> log.info("On complete"));
     }
 }
